cedadc7d86cb7f9915d5362b4aa7e914426c70e8
void314 <71975200+void314@users.noreply.github.com>
2021-12-15 08:44:02 +0600

    Add files via upload

---
+++ task5/js/script.js
@@ -0,0 +1,27 @@
+
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+window.addEventListener('keydown', function (e) {
+    const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"`);
+    //if(!audio) return;
+    //audio.currentTime = 0;
+    //audio.play();
+
+    const keys = Array.from(document.querySelectorAll('.key'));
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+    window.addEventListener('keydown', playSound);
+});
